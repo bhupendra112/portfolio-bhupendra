582c362a3c3468d4744c05da12a004b173f941f0
bhupendra gupta <134203340+bhupendra112@users.noreply.github.com>
2024-09-06 15:30:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,23 @@
 const Footer = () => {
   return (
     <>
-      <div className="py-5 border bg-gray-100 flex justify-between px-72">
-        <div>
+      <div className="py-5 border bg-gray-100 flex flex-col sm:flex-row justify-between items-center px-5 sm:px-10 lg:px-72">
+        <div className="text-center sm:text-left mb-4 sm:mb-0">
           <p>
-            @2023 Bhupendra Prasad Gupta <span>All Rights Reserved</span>
+            &copy; 2023 Bhupendra Prasad Gupta <span>All Rights Reserved</span>
           </p>
         </div>
-        <div className="space-x-5">
+        <div className="space-x-5 flex flex-wrap justify-center sm:justify-end">
           <a href="/about" className="hover:underline">
             About
           </a>
-          <a href="/about" className="hover:underline">
+          <a href="/privacy" className="hover:underline">
             Privacy Policy
           </a>
-          <a href="/about" className="hover:underline">
-            Licensing{" "}
+          <a href="/licensing" className="hover:underline">
+            Licensing
           </a>
-          <a href="/about" className="hover:underline">
+          <a href="/contact" className="hover:underline">
             Contact
           </a>
         </div>
